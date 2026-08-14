df2a0b6d5837c39038bedbde80ca7e3c3cb7aee0
Akshita Agrawal <agrawalakshita66@gmail.com>
2026-08-14 18:58:12 +0530

    patterns

--- Control_statement/patterns.cpp
+++ Control_statement/patterns.cpp
@@ -129,3 +129,224 @@ int main() {
 
     return 0;
 }
+
+
+// hollow pyramid
+    //     *
+    //    * *
+    //   *   *
+    //  *     * 
+    // *********       
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+   for(int i=1;i<=n;i++){
+        for(int j=n-i;j>0;j--){   //space
+            cout<<" ";
+        }
+        for(int j=1;j<=2*i-1;j++){  //star
+         if(j==1 || j==2*i-1 || i==n){
+            cout<<"*";
+         }
+         else{
+             cout<<" ";
+         }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+
+
+// hollow square
+// *****
+// *   *
+// *   *
+// *   *
+// *****
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+   for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){  //star
+         if(i==1 || j==1 || j==n || i==n){
+            cout<<"*";
+         }
+         else{
+             cout<<" ";
+         }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+// hollow triangle
+// *
+// **
+// * *
+// *  *
+// *****
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+   for(int i=1;i<=n;i++){
+        // for(int j=n-i;j>0;j--){   //space
+        //     cout<<" ";
+        // }
+        for(int j=1;j<=i;j++){  //star
+         if(j==1 || j==i || i==n){
+            cout<<"*";
+         }
+         else{
+             cout<<" ";
+         }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+// butterfly
+*        *
+**      **
+***    ***
+****  **** 
+**********
+****  ****
+***    ***
+**      **
+*        *
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+   for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){  //star
+            cout<<"*";
+        
+        }
+        for(int j=1;j<=2*(n-i);j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=n-1;i>=1;i--){
+        for(int j=1;j<=i;j++){  //star
+            cout<<"*";
+        
+        }
+        for(int j=1;j<=2*(n-i);j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
+
+
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+   for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){  
+            cout<<j;
+        
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
+
+
+// 1
+// 21
+// 321
+// 4321
+// 54321
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+   for(int i=1;i<=n;i++){
+       int a=i;
+        for(int j=1;j<=i;j++){  
+            cout<<a;
+            a--;
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
+
+
+
+// 12345
+// 1234
+// 123
+// 12
+// 1
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+   for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-(i-1);j++){  
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
+
+
+// 1
+// 23
+// 456
+// 78910
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a=1;
+   for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){  
+            cout<<a;
+            a++;
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
