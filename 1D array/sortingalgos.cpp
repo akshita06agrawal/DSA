64f487c582e88077b23172c3d03be4ba2fdf0fda
Akshita Agrawal <agrawalakshita66@gmail.com>
2026-09-01 18:04:24 +0530

    Refactor sorting algorithms: separate implementations for Bubble, Insertion, and Selection sort

--- 1D array/sortingalgos.cpp
+++ 1D array/sortingalgos.cpp
@@ -31,3 +31,68 @@ int main(){
     cout<<endl;
     return 0;
 }
+
+
+
+// Insertion sort
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter size of array:";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter elements of array:";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=1;i<n;i++){
+        int key=arr[i];
+        int j=i-1;
+        while(j>=0 && key<arr[j]){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+    cout<<"After sorting:";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
+
+
+
+// Selection sort
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter size of array:";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter elements of array:";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        int min=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[min]){
+                min=j;
+            }
+            swap(arr[i],arr[min]);
+        }
+        
+    }
+    cout<<"After sorting:";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
