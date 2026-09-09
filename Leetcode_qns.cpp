0014022d681ad3d083e29a7f57e67e5ad4430f1d
Akshita Agrawal <agrawalakshita66@gmail.com>
2026-09-09 23:59:03 +0530

    new qn

--- Leetcode_qns.cpp
+++ Leetcode_qns.cpp
@@ -157,3 +157,28 @@ public:
     }
 };
 
+
+
+// 2390 Removing stars from string
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char>st;
+        for(char ch:s){
+            if(ch=='*'){
+                st.pop();
+            }
+            else{
+                st.push(ch);
+            }
+        }
+        string ans="";
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
