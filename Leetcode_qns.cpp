9e2c82e66e2dd10e3457f3aafba97d2176bb2cef
Akshita Agrawal <agrawalakshita66@gmail.com>
2026-09-13 23:46:56 +0530

    new

--- Leetcode_qns.cpp
+++ Leetcode_qns.cpp
@@ -231,3 +231,21 @@ public:
     }
 };
 
+
+// 58. Length of last word
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i=s.length()-1;
+        int len=0;
+        while(i>=0 && s[i]==' '){
+            i--;
+        }
+        while(i>=0 && s[i]!=' '){
+            len++;
+            i--;
+        }
+        return len;
+    }
+};
+
