61266cf9caad622963a0a5b58242ff09a9ad1035
Akshita Agrawal <agrawalakshita66@gmail.com>
2026-09-17 23:56:31 +0530

    Another string question

--- Leetcode_qns.cpp
+++ Leetcode_qns.cpp
@@ -338,3 +338,18 @@ public:
         sort(nums1.begin(),nums1.end());
     }
 };
+
+
+
+// 1903. Largest Odd number in a string 
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        for (int i = num.size() - 1; i >= 0; i--) {
+            if ((num[i] - '0') % 2 != 0) {
+                return num.substr(0, i + 1);
+            }
+        }
+        return "";
+    }
+};
