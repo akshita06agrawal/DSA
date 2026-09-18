527029e468cfc13cb454707a8566e0bbd2ef5e86
Akshita Agrawal <agrawalakshita66@gmail.com>
2026-09-18 23:45:05 +0530

    Find sqrt of a number

--- Leetcode_qns.cpp
+++ Leetcode_qns.cpp
@@ -353,3 +353,29 @@ public:
         return "";
     }
 };
+
+
+
+// 69. Sqrt(x)
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0 || x==1){
+            return x;
+        }
+        int left=0;
+        int right=x;
+        int ans=0;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(mid<=x/mid){
+                ans=mid;
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return ans;
+    }
+};
