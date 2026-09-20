fa69dee6612510385a9a721e4380d94e99f02b23
Akshita Agrawal <agrawalakshita66@gmail.com>
2026-09-20 23:14:55 +0530

    Add implementation for Valid Parentheses problem using stack

--- recursion.cpp
+++ recursion.cpp
@@ -270,3 +270,28 @@ int main()
     }
     return 0;
 }
+
+
+
+// 20. Valid parentheses
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char ch : s) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            } else {
+                if (st.empty()) {
+                    return false;
+                }
+                char top = st.top();
+                st.pop();
+                if (ch == ')' && top != '(') return false;
+                if (ch == ']' && top != '[') return false;
+                if (ch == '}' && top != '{') return false;
+            }
+        }
+        return st.empty();
+    }
+};
