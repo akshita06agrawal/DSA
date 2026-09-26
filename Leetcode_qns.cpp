08f4d639393c586cec8310b652eb6166b432e402
Akshita Agrawal <agrawalakshita66@gmail.com>
2026-09-26 23:59:37 +0530

    leetcode qn

--- Leetcode_qns.cpp
+++ Leetcode_qns.cpp
@@ -701,3 +701,24 @@ public:
         }
     }
 };
+
+
+
+
+
+
+
+// 283. Move Zeroes
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int left = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+            if (nums[right] != 0) {
+                swap(nums[right], nums[left]);
+                left++;
+            }
+        }        
+    }
+};
